c0d608e9a6bc34f35fd9c4a47efbbb4ebc36c4a3
Try43-loki <trykhomrean43@gmail.com>
2025-02-13 19:55:12 +0700

    All format

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ const paragraph = document.querySelector("#paragraph");
 const notification = document.querySelector("#notification");
 let selectValue;
 let isSelected = true;
+
+// select option
 selection.addEventListener("change", (e) => {
   e.preventDefault();
   selectValue = selection.value;
@@ -39,8 +41,10 @@ selection.addEventListener("change", (e) => {
   }
 });
 
+// form submit
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  // check value 
   let searchValue;
   if(isSelected) {
     searchValue = Number(search.value);
@@ -49,31 +53,41 @@ form.addEventListener("submit", (e) => {
   }
 
   if (searchValue === 1 || searchValue == "Positive Thinkings") {
+    
     img.classList.remove("hidden");
     title.innerHTML = "Positive Thinkings";
     paragraph.innerHTML = `Say something positive, and you'll see something positive. - Jim Thompson`;
     img.src = "img/positive.jpg";
     notification.innerHTML = "";
+
   } else if (searchValue === 2 || searchValue == "Keep doing") {
+    
     img.classList.remove("hidden");
     img.src = "img/keeping.jpg";
     title.innerHTML = "keep doing";
     paragraph.innerHTML = `What's done is done. What's gone is gone. One of life's lessons is always moving on. It’s okay to look back to see how far you’ve come but keep moving forward.
                           ― Roy T. Bennett.`;
     notification.innerHTML = "";
+  
   } else if (searchValue === 3 || searchValue == "Dream") {
+  
     img.classList.remove("hidden");
     img.src = "img/dream.jpg";
     title.innerHTML = "Dream";
     paragraph.innerHTML = `A dream doesn't become reality through magic; it takes sweat, determination and hard work. - Colin Powell`;
     notification.innerHTML = "";
+
   } else {
+
     notification.innerHTML = "No result found";
     img.classList.add("hidden");
     title.innerHTML = "";
     paragraph.innerHTML = "";
+
   }
 });
+
+// clear form
 const clear = ()=>{
   search.value = "";
   img.classList.add("hidden");
